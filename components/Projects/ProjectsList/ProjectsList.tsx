bc946dba7f9e222ffae28931f8223dbd578e9599
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-12-13 11:38:13 +0100

    feat: add sample projects

--- components/Projects/ProjectsList/ProjectsList.tsx
+++ components/Projects/ProjectsList/ProjectsList.tsx
@@ -1,24 +1,39 @@
 import { Table, Tag } from 'antd'
 import Column from 'antd/lib/table/Column'
 import { getTagColor } from '@utils/getTagColor'
+import Link from 'next/link'
 
 const ProjectsList = () => {
   const data = [
     {
       key: '1',
       name: 'Sample project 1',
-      state: 'close'
+      state: 'close',
+      team: {
+        name: 'Sample team 1',
+        link: '/teams/1'
+      }
     },
     {
       key: '2',
       name: 'Sample project 2',
-      state: 'open'
+      state: 'open',
+      team: {
+        name: 'Sample team 2',
+        link: '/teams/1'
+      }
     }
   ]
 
   return (
     <Table dataSource={data}>
       <Column title="Name" dataIndex="name" key="name" />
+      <Column
+        title="Team"
+        dataIndex="team"
+        key="team"
+        render={(team) => <Link href={team.link}>{team.name}</Link>}
+      />
       <Column
         title="State"
         dataIndex="state"
