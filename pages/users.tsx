c845405668066e990d16cddd82cde8c0dfe5f7e3
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-01-19 20:02:54 +0100

    feat: add users page

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,16 +1,35 @@
 import Head from 'next/head'
+import UsersApi from 'services/Api/Users'
+import UsersList from '@components/Users/UsersList'
+import { GetStaticProps } from 'next'
+import { IUsers } from 'types/IUsers'
 import { Typography } from 'antd'
 
 const { Title } = Typography
 
-const Home: React.FC = () => (
+interface IProps {
+  users: IUsers
+}
+
+const Users: React.FC<IProps> = ({ users }) => (
   <>
     <Head>
       <title>Users</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <Title>Users</Title>
+    {users && <UsersList users={users} />}
   </>
 )
 
-export default Home
+export const getStaticProps: GetStaticProps = async () => {
+  const users = (await new UsersApi().get()).data
+
+  return {
+    props: {
+      users
+    }
+  }
+}
+
+export default Users
