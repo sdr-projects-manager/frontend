cf7cffc1c2937dce7222ab19383dc333bc8d7334
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-02-01 20:26:56 +0100

    chore: configure 18n routing for pl and en

--- next.config.js
+++ next.config.js
@@ -1,19 +1,29 @@
 const withLess = require('@zeit/next-less')
 const withCSS = require('@zeit/next-css')
+const compose = require('next-compose')
 
-module.exports = withCSS(
-  withLess({
-    lessLoaderOptions: {
-      modifyVars: {
-        'primary-color': '#3CEAB8',
-        'link-color': '#3CEAB8',
-        'layout-header-background': '#17191D',
-        'text-color-secondary': '#3CEAB8'
-      },
-      javascriptEnabled: true
-    },
-    webpack(config, options) {
-      return config
+module.exports = compose([
+  {
+    i18n: {
+      locales: ['en', 'pl'],
+      defaultLocale: 'en'
     }
-  })
-)
+  },
+  [
+    withCSS,
+    withLess({
+      lessLoaderOptions: {
+        modifyVars: {
+          'primary-color': '#3CEAB8',
+          'link-color': '#3CEAB8',
+          'layout-header-background': '#17191D',
+          'text-color-secondary': '#3CEAB8'
+        },
+        javascriptEnabled: true
+      },
+      webpack(config, options) {
+        return config
+      }
+    })
+  ]
+])
