2339dab895a8d66ad248c3d5f166f65daa1ab921
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-02-03 18:54:17 +0100

    feat: add react-toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import 'react-toastify/dist/ReactToastify.css'
 import GlobalStyles from '@styles/GlobalStyles'
 import NProgress from 'nprogress'
 import Router from 'next/router'
@@ -5,6 +6,7 @@ import SwitchTemplate from '@components/templates/SwitchTemplate'
 import store from '@store/store'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
 import { appWithTranslation } from '../locale/i18n'
 
 NProgress.configure({ showSpinner: false })
@@ -15,6 +17,7 @@ Router.events.on('routeChangeError', () => NProgress.done())
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <Provider store={store}>
     <GlobalStyles />
+    <ToastContainer />
     <SwitchTemplate Component={Component} pageProps={pageProps} />
   </Provider>
 )
