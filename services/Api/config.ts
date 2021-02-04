50f790266a76d11f208a2e93da27fd74585d322b
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-02-04 17:45:35 +0100

    feat: add axios authorisation with token

--- services/Api/config.ts
+++ services/Api/config.ts
@@ -1,8 +1,28 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
+import LocalStorageService from 'services/LocalStorageService'
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: process.env.API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 })
+
+function createAxiosResponseInterceptor(instance: AxiosInstance) {
+  instance.interceptors.request.use(
+    (config) => {
+      const token = LocalStorageService.getToken()
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
+    },
+    (error) => {
+      Promise.reject(error)
+    }
+  )
+}
+
+createAxiosResponseInterceptor(axiosInstance)
+
+export { axiosInstance }
