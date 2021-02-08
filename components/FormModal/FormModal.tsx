f73df463bc4bb22747da03841fce90ee20d76c1c
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-02-08 18:24:55 +0100

    feat: add FormModal

---
+++ components/FormModal/FormModal.tsx
@@ -0,0 +1,37 @@
+import Modal from 'antd/lib/modal/Modal'
+import { Button } from 'antd'
+import { PlusCircleFilled, EditFilled } from '@ant-design/icons'
+import { useState } from 'react'
+import { withTranslation } from 'locale/i18n'
+
+interface IProps {
+  t: (text: string) => string
+  FormComponent: React.ReactNode
+  type: 'add' | 'edit'
+}
+
+const FormModal: React.FC<IProps> = ({ t, FormComponent, type }) => {
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <>
+      <Button
+        type={type === 'add' ? 'primary' : 'link'}
+        icon={type === 'add' ? <PlusCircleFilled /> : <EditFilled />}
+        onClick={() => setOpen(true)}
+      >
+        {type === 'add' ? t('Add') : t('Edit')}
+      </Button>
+      <Modal
+        title={type === 'add' ? t('Add') : t('Edit')}
+        visible={isOpen}
+        onCancel={() => setOpen(false)}
+        footer={<></>}
+      >
+        {FormComponent}
+      </Modal>
+    </>
+  )
+}
+
+export default withTranslation('common')(FormModal)
