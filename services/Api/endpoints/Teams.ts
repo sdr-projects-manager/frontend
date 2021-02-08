61f4d7da1da847919640c98aeb69ccc947121891
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-02-08 18:22:45 +0100

    feat: add teams api

---
+++ services/Api/endpoints/Teams.ts
@@ -0,0 +1,20 @@
+import { ITeam, ITeams } from '@types/ITeams'
+import { AxiosResponse } from 'axios'
+import { axiosInstance } from '../config'
+import { IApi } from '../IApi'
+
+export default class Teams implements IApi {
+  base = 'teams/'
+
+  async get(): Promise<AxiosResponse<ITeams>> {
+    return axiosInstance.get(this.base)
+  }
+
+  async getById(id: number): Promise<AxiosResponse<ITeam>> {
+    return axiosInstance.get(`${this.base}${id}`)
+  }
+
+  async add(data: Partial<ITeams>) {
+    return axiosInstance.post(`${this.base}`, data)
+  }
+}
